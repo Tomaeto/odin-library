adf1c3bad8aff3f4038fe198d7dc4a96df0d71d8
Adrian Faircloth <adrianspenser@gmail.com>
2026-09-01 12:36:18 -0400

    Add basic table functionality

--- script.js
+++ script.js
@@ -13,15 +13,49 @@ class Book {
     getInfo() {
         return `${this.title} by ${this.author}, ${this.pageCount} pages, ${this.read}`;
     }
+    getTitle() {
+        return this.title;
+    }
+    getAuthor() {
+        return this.author;
+    }
+    getPageCount() {
+        return this.pageCount;
+    }
+    getRead() {
+        return this.read;
+    }
+    getID() {
+        return this.id;
+    }
 }
 
-const lib = [];
 function addBookToLibrary() {
     const book = new Book(window.prompt("Title: "), window.prompt("Author: "), window.prompt("Page count: "));
     lib.push(book);
 }
 
+function addBookToTable(table, book) {
+    const row = table.insertRow();
+    let titleCell = row.insertCell(0);
+    let authorCell = row.insertCell(1);
+    let pageCell = row.insertCell(2);
+    let readCell = row.insertCell(3);
+    titleCell.innerText = book.getTitle();
+    authorCell.innerText = book.getAuthor();
+    pageCell.innerText = book.getPageCount();
+    readCell.innerText = book.getRead();
+}
+
+function addAllBooksToTable(table, lib) {
+
+    for (const book of lib) {
+        addBookToTable(table, book);
+    }
+}
+
+const lib = [];
 const book = new Book("Title", "Author", 12);
-console.log(book.getInfo());
-book.toggleRead();
-console.log(book.getInfo());
+lib.push(book);
+const table = document.getElementById("myTable");
+addAllBooksToTable(table, lib);
